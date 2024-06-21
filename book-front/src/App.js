bdb7d082ade57d0dfdbaddce05f345e232899e92
yoshisuetakuya <t.yoshisue@cybergrow.co.jp>
2024-06-21 13:52:08 +0900

    コメントの修正

--- book-front/src/App.js
+++ book-front/src/App.js
@@ -13,21 +13,21 @@ function App() {
   const [searchWord, setSearchWord] = useState("");
   const [sortState, setSortState] = useState("");
 
-  //初回レンダリングで全件を表示する
+   // 初回レンダリング時にデータを全件取得する
   useEffect(() => {
-    fetch("http://localhost:8080/Get")  //fetchメソッドでhttp://localhost:8080/Getにデータを取得しに行く
-      .then((res) => res.json())        //ここでjson形式のデータを取得
-      .then((json) => setData(json))    //取得したデータをsetDataにセット
-      .catch((error) => console.log("Error:", error));
-  }, []);                               //useEffectで第二引数に[]を渡し初回レンダリング時に実行させる
+    fetch("http://localhost:8080/Get")  // fetchメソッドでサーバーからデータを取得
+      .then((res) => res.json())        // 取得したレスポンスをjson形式に変換
+      .then((json) => setData(json))    // jsonデータをセット
+      .catch((error) => console.log("Error:", error));     // エラーが発生した場合の処理
+  }, []); // 第二引数に[]を渡すことで初回レンダリング時のみ実行
 
   // 本の追加処理
   function handleCreate() {
     fetch("http://localhost:8080/Create", {
-      method: 'POST',
-      body: JSON.stringify({ "title": title, "author": author, "impression": impression }),
+      method: 'POST', // POSTメソッドを指定
+      body: JSON.stringify({ "title": title, "author": author, "impression": impression }),// リクエストボディに新規本データをセット
       headers: {
-        'Content-type': 'application/json',
+        'Content-type': 'application/json',// リクエストヘッダーをJSON形式に指定
       },
     })
       .then((json) => {
@@ -50,11 +50,11 @@ function App() {
   // 本の削除処理
   const handleDelete = (bookid) => {
     fetch(`http://localhost:8080/Delete/${bookid}`, {
-      method: 'DELETE',
+      method: 'DELETE',// DELETEメソッドを指定
     })
       .then((json) => {
         console.log(json);
-        // 削除している
+          // 削除後、データをフィルタリングして更新する
         setData(data.filter(item => item.bookid !== bookid));
       })
       .catch((error) => console.log("Error:", error));
@@ -63,10 +63,10 @@ function App() {
   // 本の更新処理
   const handleUpdate = (bookid) => {
     fetch(`http://localhost:8080/Update/${bookid}`, {
-      method: 'PUT',
-      body: JSON.stringify({ "bookid": bookid, "title": updateTitle, "author": updateAuthor, "impression": updateImpression }),
+      method: 'PUT',// PUTメソッドを指定
+      body: JSON.stringify({ "bookid": bookid, "title": updateTitle, "author": updateAuthor, "impression": updateImpression }), // リクエストボディに更新データをセット
       headers: {
-        'Content-type': 'application/json',
+        'Content-type': 'application/json',// リクエストヘッダーをJSON形式に指定
       },
     })
       .then((json) => {
@@ -79,8 +79,8 @@ function App() {
             return item;
           }
         });
-        setData(updateData);
-        setEditingId(null);//新しく更新した後にsetEditingIdにnullをセットしておくことで編集のボタンに変わる
+        setData(updateData);// 更新後のデータをセット
+        setEditingId(null);// 更新後、nullを渡して編集モードを解除する
       })
       .catch((error) => console.log("Error:", error));
   }
@@ -88,18 +88,18 @@ function App() {
   //本の検索処理
   function handleSearch() {
     fetch("http://localhost:8080/Search", {
-      method: 'POST',
-      body: JSON.stringify({ "keyword": searchWord }),
+      method: 'POST',// POSTメソッドを指定
+      body: JSON.stringify({ "keyword": searchWord }), // リクエストボディに検索キーワードをセット
       headers: {
-        'Content-type': 'application/json',
+        'Content-type': 'application/json',// リクエストヘッダーをJSON形式に指定
       },
     })
       .then((res) => res.json())
       .then((json) => {
         console.log(json);
-        setData(json)
+        setData(json)// 検索結果をセットする
         setSearchWord(""); // 入力フォームを空にする
-        setSortState("") // ソートしたまま検索をかけたときに元の並び順(bookid)に戻す
+        setSortState("") // // ソート状態をリセットする
       })
       .catch((error) => console.error('Error:', error));
   };
@@ -107,33 +107,29 @@ function App() {
   // タイトルでソートする機能
   function handleTitleSort() {
     if (sortState !== "ascTitle") {
-      // 現在のソート状態が「タイトルの昇順」ではない場合
-      // データをタイトルの昇順にソートします
+      // 昇順にソートする
       const sortedData = [...data].sort((a, b) => (a.title < b.title ? -1 : 1));
-      setData(sortedData); // ソートされたデータを更新します
-      setSortState("ascTitle"); // ソート状態を「タイトルの昇順」に設定します
-    } else {
-      // 現在のソート状態が「タイトルの昇順」である場合
-      // データをタイトルの降順にソートします
+      setData(sortedData); // ソートされたデータを更新する
+      setSortState("ascTitle"); // ソート状態を昇順に設定する
+    } else if (sortState === "ascTitle") {
+      // 降順にソートする
       const sortedData = [...data].sort((a, b) => (a.title > b.title ? -1 : 1));
-      setData(sortedData); // ソートされたデータを更新します
-      setSortState("descTitle"); // ソート状態を「タイトルの降順」に設定します
+      setData(sortedData); // ソートされたデータを更新する
+      setSortState("descTitle"); // ソート状態を降順に設定する
     }
   }
   // 作者でソートする機能
   function handleAuthorSort() {
     if (sortState !== "ascAuthor") {
-      // 現在のソート状態が「作者の昇順」ではない場合
-      // データを作者の昇順にソートします
+      // 昇順にソートする
       const sortedData = [...data].sort((a, b) => (a.author < b.author ? -1 : 1));
-      setData(sortedData); // ソートされたデータを更新します
-      setSortState("ascAuthor"); // ソート状態を「作者の昇順」に設定します
-    } else {
-      // 現在のソート状態が「作者の昇順」である場合
-      // データを作者の降順にソートします
+      setData(sortedData); // ソートされたデータを更新する
+      setSortState("ascAuthor"); // ソート状態を昇順設定する
+    } else if (sortState === "ascAuthor") {
+      // 降順にソートします
       const sortedData = [...data].sort((a, b) => (a.author > b.author ? -1 : 1));
-      setData(sortedData); // ソートされたデータを更新します
-      setSortState("descAuthor"); // ソート状態を「作者の降順」に設定します
+      setData(sortedData); // ソートされたデータを更新する
+      setSortState("descAuthor"); // ソート状態を降順設定する
     }
   }
 
@@ -157,7 +153,7 @@ function App() {
             <th>
               {(() => {
                 if (sortState === "ascTitle") {
-                  return <button onClick={handleTitleSort}>タイトル ↑</button>;
+                  return <button onClick={handleTitleSort} >タイトル ↑</button>;
                 } else if (sortState === "descTitle") {
                   return <button onClick={handleTitleSort}>タイトル ↓</button>;
                 } else {
@@ -185,32 +181,33 @@ function App() {
           {data.map((item) => (
             <tr key={item.bookid}>
               <td>
-                {editingId === item.bookid ? (//編集のボタンをクリックでテキストボックスを表示
+                {editingId === item.bookid ? ( // 編集モード時にテキストボックスを表示
                   <input placeholder="タイトルを入力して更新" value={updateTitle} onChange={(event) => setUpdateTitle(event.target.value)} />
                 ) : (
-                  item.title //nullの時(初期値の時)はタイトルを表示
+                  item.title // 通常時はタイトルを表示
                 )}
               </td>
               <td>
-                {editingId === item.bookid ? (//編集のボタンをクリックででテキストボックスを表示
+                {editingId === item.bookid ? (// 編集モード時にテキストボックスを表示
                   <input placeholder="作者を入力して更新" value={updateAuthor} onChange={(event) => setUpdateAuthor(event.target.value)} />
                 ) : (
-                  item.author //nullの時(初期値の時)は作者を表示
+                  item.author // 通常時は作者を表示
                 )}
               </td>
               <td>
-                {editingId === item.bookid ? (//編集のボタンをクリックでテキストボックスを表示
+                {editingId === item.bookid ? (// 編集モード時にテキストボックスを表示
                   <input placeholder="ひとこと感想を入力して更新" value={updateImpression} onChange={(event) => setUpdateImpression(event.target.value)} />
                 ) : (
-                  item.impression //nullの時(初期値の時)はひとこと感想を表示
+                  item.impression // 通常時は感想を表示
                 )}
               </td>
               <td><button onClick={() => handleDelete(item.bookid)}>削除</button></td>
               <td>
-                {editingId === item.bookid ? (//該当行の「編集ボタン」クリックで「更新ボタン」に表示を変えてクリックで更新できるようにする
+                {editingId === item.bookid ? ( // 編集モード時は「更新ボタン」を表示
                   <button onClick={() => handleUpdate(item.bookid)}>更新</button>
                 ) : (
-                  <button onClick={() => {//setEditingIdがnullの時(初期値)「編集ボタン」を表示しクリックで該当行のにbookidをsetEditingIdセットして初期値に本の情報をセットする
+                  <button onClick={() => {
+                    // 編集ボタンをクリックで編集モードに入り、初期値に本の情報をセット
                     setEditingId(item.bookid);
                     setUpdateTitle(item.title);
                     setUpdateAuthor(item.author);
